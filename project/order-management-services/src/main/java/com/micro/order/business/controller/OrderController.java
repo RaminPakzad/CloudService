681306ab47a7568efc4cf41c90d01c118aa4b00c
farzinpashaee <f.pashaee@yahoo.com>
2019-06-30 08:46:59 +0430

    ADD: Services and endpoints added

--- project/order-management-services/src/main/java/com/micro/order/business/controller/OrderController.java
+++ project/order-management-services/src/main/java/com/micro/order/business/controller/OrderController.java
@@ -1,6 +1,8 @@
 package com.micro.order.business.controller;
 
+import com.micro.order.business.service.OrderService;
 import com.micro.order.model.entity.Order;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -10,19 +12,27 @@ import java.util.List;
 @RequestMapping("/order/")
 public class OrderController {
 
+    @Autowired
+    OrderService orderService;
+
     @GetMapping(value="/listAll")
     public List<Order> listAll(){
-        return new ArrayList<>();
+        return orderService.listAll();
     }
 
     @GetMapping(value="/findById/{id}")
-    public Order findById(@PathVariable int id){
-        return new Order();
+    public Order findById(@PathVariable long id){
+        return orderService.findById(id);
     }
 
     @PostMapping(value="/add")
-    public void add(@RequestBody Order order){
-        // add new order
+    public Order add(@RequestBody Order order){
+        return orderService.add(order);
+    }
+
+    @DeleteMapping(value="/delete/{id}")
+    public void delete(@PathVariable long id){
+        orderService.delete( id );
     }
 
 }
--- project/order-management-services/src/main/java/com/micro/order/business/service/OrderService.java
+++ project/order-management-services/src/main/java/com/micro/order/business/service/OrderService.java
@@ -2,6 +2,7 @@ package com.micro.order.business.service;
 
 import com.micro.order.business.repository.OrderRepository;
 import com.micro.order.model.entity.Order;
+import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +17,8 @@ public class OrderService {
     public List<Order> listAll(){
         return (List<Order>) orderRepository.findAll();
     }
+    public Order findById( long id ) { return orderRepository.findById(id).get(); }
+    public Order add( Order order ){ return orderRepository.save(order); }
+    public void delete( long id ) { orderRepository.delete(orderRepository.findById(id).get()); }
 
 }
